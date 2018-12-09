d527fe9590f0ab6f1231e8eb1b796cc24a739e22
Ilyas Assainov <iliyas.assainov@gmail.com>
2018-12-09 17:11:54 +0400

    Updates

---
+++ precache-manifest.d8dfceb63ac359a7cf4219c58ce74d98.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "58c0c91e5d9bbb7b8c69",
+    "url": "/industrious/static/js/runtime~main.58c0c91e.js"
+  },
+  {
+    "revision": "b591c14268f50fab506d",
+    "url": "/industrious/static/js/main.b591c142.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/industrious/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "0867a4baaa265ad0606451656fd2b3c7",
+    "url": "/industrious/index.html"
+  }
+];
